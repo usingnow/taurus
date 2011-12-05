ad0b9993ad8035567877283dda6484e97e338e06
Jianmin Xu <jianmin.xu@usingnow.com>
2011-12-05 18:58:19 +0800

    create product scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ebiz::Application.routes.draw do
+  resources :products
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/schema.rb
@@ -0,0 +1,60 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20111205081958) do
+
+  create_table "products", :force => true do |t|
+    t.string   "product_id"
+    t.string   "name"
+    t.decimal  "cost_bef_tax",              :precision => 8, :scale => 2
+    t.decimal  "cost_aft_tax",              :precision => 8, :scale => 2
+    t.string   "brand_id"
+    t.string   "product_category_id"
+    t.string   "unit"
+    t.string   "specification"
+    t.integer  "delivery_days"
+    t.integer  "type"
+    t.string   "model"
+    t.string   "size"
+    t.string   "delivery_descprition"
+    t.text     "description"
+    t.string   "color"
+    t.boolean  "can_on_behalf"
+    t.string   "barcode"
+    t.string   "supplier_id"
+    t.string   "manufacturer"
+    t.boolean  "have_shelf_life"
+    t.integer  "shelf_life"
+    t.boolean  "have_wanrranty"
+    t.integer  "wanrranty"
+    t.text     "wanrranty_description"
+    t.string   "origin"
+    t.string   "appendant"
+    t.boolean  "is_green"
+    t.boolean  "is_assembling_required"
+    t.decimal  "assembling_fee_bef_tax",    :precision => 8, :scale => 2
+    t.decimal  "assembling_fee_aft_tax",    :precision => 8, :scale => 2
+    t.boolean  "is_maintanable"
+    t.decimal  "maintenance_cost_bef_tax",  :precision => 8, :scale => 2
+    t.decimal  "maintenance_cost_aft_tax",  :precision => 8, :scale => 2
+    t.boolean  "is_installation_required"
+    t.decimal  "installation_cost_bef_tax", :precision => 8, :scale => 2
+    t.decimal  "insatllation_cost_aft_tax", :precision => 8, :scale => 2
+    t.time     "create_time"
+    t.string   "created_by"
+    t.time     "update_time"
+    t.string   "updated_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
