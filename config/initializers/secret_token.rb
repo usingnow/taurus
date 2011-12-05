96eaceb3d56616bac8eeb47c8c9158c1d43b1532
zhang xiangyu <xiangyu.zhang@usingnow.com>
2011-12-05 13:52:25 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebiz::Application.config.secret_token = 'd6adf2ef19f84155103319390cc186430123c461ddad7a9038865fee6b5b191c507c98268f05b90a0fba57e2cf56ea579b18ed8e9d0c69d325aef8f9bbac33c5'
