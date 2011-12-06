f9d024e57f665e9a9a2555004b4ab4cfea16c55f
zhang xiangyu <xiangyu.zhang@usingnow.com>
2011-12-06 18:11:10 +0800

    stash2

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,4 @@
 class Admin < ActiveRecord::Base
-  validates :login_no, :password, :presence => true
-  validates :email,:format => {:with=>/@/,:message=>'123'}
+  validates :login_no, :password,:question,:answer,:name, :presence => true
+  validates :email, :email => {:message => :email}
 end
--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Ebiz
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -28,7 +29,7 @@ module Ebiz
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :cn
+    config.i18n.default_locale = :'zh-CN'
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
