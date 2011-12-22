6ac26eee083136f42e228326a17592673e79f031
zhang xiangyu <xiangyu.zhang@usingnow.com>
2011-12-22 13:25:55 +0800

    refs #88

--- spec/controllers/admin/procedures_controller_spec.rb
+++ spec/controllers/admin/procedures_controller_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe ProceduresController do
+describe Admin::ProceduresController do
 
   # This should return the minimal set of attributes required to create a valid
   # Procedure. As you add validations to Procedure, be sure to
