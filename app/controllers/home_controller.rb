9564c4e2c60c4519cfffa5010ef1bccaa7aaac94
sean <xiangyu.zhang@usingnow.com>
2012-01-12 09:48:31 +0800

    首页

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,5 @@
 class HomeController < ActionController::Base
   def index
-     @skus = Sku.all
      @pick_ups = PickUp.all
-
-     respond_to do |format|
-       format.html #index.html.erb
-     end
   end
 end
