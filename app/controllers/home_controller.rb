ecced36133f61889c7c5907e348f44281e5fe0d9
sean <xiangyu.zhang@usingnow.com>
2012-01-12 16:28:52 +0800

    oa_pc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ActionController::Base
   def index
     @slider_bars = SliderBar.all
     @panic_buyings = PanicBuying.all
+    @skus = Sku.limit(8).all
   end
 end
--- app/models/sku.rb
+++ app/models/sku.rb
@@ -1,4 +1,5 @@
 class Sku < ActiveRecord::Base
   has_many :order_details
+  has_many :sku_images
   validates :number, :name, :brand_id, :sku_category_id, :unit, :specification, :delivery_day, :presence => true
 end
