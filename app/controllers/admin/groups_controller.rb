b9f61604693d817059bf4d13228981ea5dd6caf4
sean <xiangyu.zhang@usingnow.com>
2012-01-15 20:01:22 +0800

    group

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -10,6 +10,10 @@ class Admin::GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   def create
     @group = Group.new params[:group]
 
@@ -18,5 +22,21 @@ class Admin::GroupsController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update_attributes params[:group]
+      redirect_to admin_groups_url
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to admin_groups_url
   end
 end
