e5e4a5d43ab1b2c8a62d75cbb1b0c30e0a75ae65
sean <xiangyu.zhang@usingnow.com>
2012-01-25 18:14:26 +0800

    fixes #236

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Ebiz::Application.initialize!
 
-
+Date::DATE_FORMATS.merge!(:default => "%Y-%m-%d")
+Time::DATE_FORMATS.merge!(:default => "%Y-%m-%d %H:%M:%S")
