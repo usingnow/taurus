562c7f696b22fad3dfb453d1c64d10fa5fd43976
sean <xiangyu.zhang@usingnow.com>
2012-02-05 14:40:42 +0800

    暂存

--- app/models/slider_bar.rb
+++ app/models/slider_bar.rb
@@ -1,5 +1,5 @@
 class SliderBar < ActiveRecord::Base
   has_attached_file :image,
-                    :url  => "/assets/slider_bars/:id_:basename.:extension",
-                    :path => ":rails_root/public/assets/slider_bars/:id_:basename.:extension"
+                    :url  => "/assets/slider_bars/:id.:extension",
+                    :path => ":rails_root/public/assets/slider_bars/:id.:extension"
 end
