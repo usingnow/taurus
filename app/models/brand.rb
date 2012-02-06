98e632d5bec0141e51f96d3de4c79f3b12612c08
sean <xiangyu.zhang@usingnow.com>
2012-02-06 12:18:41 +0800

    修复assets

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,6 +1,6 @@
 class Brand < ActiveRecord::Base
   has_attached_file :image,
-                    :url  => "/assets/brands/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/assets/brands/:id/:style_:basename.:extension"
+                    :url  => "/paperclip_assets/brands/:id/:style_:basename.:extension",
+                    :path => ":rails_root/public/paperclip_assets/brands/:id/:style_:basename.:extension"
   validates :chinese_name, :company_name, :presence => true
 end
