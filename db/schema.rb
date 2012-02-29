ebc87222d3262ef473a2efd6aa48e1c2c71a8064
Jimmy Xu <jianmin.xu@usingnow.com>
2012-02-29 21:47:57 +0800

    增加前台按钮样式。

--- db/schema.rb
+++ db/schema.rb
@@ -937,6 +937,7 @@ ActiveRecord::Schema.define(:version => 20120229085123) do
     t.integer  "role_id"
     t.integer  "status"
     t.integer  "user_type"
+    t.string   "email",                                 :default => "", :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -949,7 +950,6 @@ ActiveRecord::Schema.define(:version => 20120229085123) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "store_id"
-    t.string   "email",                                 :default => "", :null => false
   end
 
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
