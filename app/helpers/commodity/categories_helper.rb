8f4b28a8f8a18b28a16e0656ebdf86d7fc9b276b
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-03-02 13:55:54 +0800

    fixes #319   搜索或者分类页面,增加以该分类为关键字做分类搜索的链接。

--- app/helpers/commodity/categories_helper.rb
+++ app/helpers/commodity/categories_helper.rb
@@ -1,17 +1,19 @@
 #encoding:UTF-8
 module Commodity::CategoriesHelper
-  def get_category_search_categories(object)
+  def get_category_search_categories
     html = ''
-    if object.number.to_s.length == 4
-      html += content_tag(:h3, object.parent.name)
-      html += content_tag(:h4, object.name, :class => "indent_20")
-      object.children.index_display.each do |child|
-        html += content_tag(:h4, child.name, :class => "indent_40")
+    parent = @sku_category.parent
+    if @sku_category.number.to_s.length == 4
+      html += content_tag(:h3, (link_to parent.name, commodity_category_path(parent.id)))
+      html += content_tag(:h4, (link_to @sku_category.name, commodity_category_path(@sku_category.id)), :class => "indent_20")
+      @sku_category.children.index_display.each do |child|
+        html += content_tag(:h4, (link_to child.name, commodity_category_path(child.id)), :class => "indent_40")
       end
     else
-      html += content_tag(:h3, object.parent.parent.name)
-      html += content_tag(:h4, object.parent.name, :class => "indent_20")
-      html += content_tag(:h4, object.name, :class => "indent_40")
+      grandfather = parent.parent
+      html += content_tag(:h3, (link_to grandfather.name, commodity_category_path(grandfather.id)))
+      html += content_tag(:h4, (link_to parent.name, commodity_category_path(parent.id)), :class => "indent_20")
+      html += content_tag(:h4, (link_to @sku_category.name, commodity_category_path(@sku_category.id)), :class => "indent_40")
     end
     html.html_safe
   end
--- app/helpers/commodity/index/skus_helper.rb
+++ app/helpers/commodity/index/skus_helper.rb
@@ -1,16 +1,16 @@
 #encoding:UTF-8
 module Commodity::Index::SkusHelper
-  def sku_search_categories(objects,keywords)
+  def sku_search_categories
     html = ''
-    objects.each do |category|
-      count = category.sku_count_by_keywords keywords
+    @categories.each do |category|
+      count = category.sku_count_by_keywords @keywords
       if count > 0
-        html += content_tag(:h4, "#{category.name}（#{count}）")
+        html += content_tag(:h4, (link_to "#{category.name}（#{count}）", commodity_category_path(category.id)))
         li = ''
         category.children.each do |child|
-          child_count = child.sku_count_by_keywords keywords
+          child_count = child.sku_count_by_keywords @keywords
           if child_count > 0
-            li += content_tag(:li, "#{child.name}（#{child_count}）")
+            li += content_tag(:li, (link_to "#{child.name}（#{child_count}）", commodity_category_path(child.id)))
           end
         end
         html += content_tag(:ul, li.html_safe)
