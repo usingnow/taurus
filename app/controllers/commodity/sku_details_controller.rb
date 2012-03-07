fd57d0fb93fc37b35c253c4981d853ef684029a7
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-03-08 00:43:47 +0800

    fixes #366,#364 修复浏览记录以时间为排序方式。

--- app/controllers/commodity/sku_details_controller.rb
+++ app/controllers/commodity/sku_details_controller.rb
@@ -9,7 +9,7 @@ class Commodity::SkuDetailsController < ApplicationController
     else
       @sku_browsing_history = @sku.add_browsing_history(current_user.id)
       @sku_browsing_history.save
-      @sku_browsing_histories = SkuBrowsingHistory.limit(5).order("quantity desc").where("user_id = #{current_user.id}
+      @sku_browsing_histories = SkuBrowsingHistory.limit(5).order("updated_at desc").where("user_id = #{current_user.id}
         and sku_id in(select id from skus where status = 1 and id in(select sku_id from sku_on_shelves where status = 1))")
     end
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,7 +20,7 @@ class HomeController < ApplicationController
     if current_user.nil?
       @sku_browsing_histories = Hash.new
     else
-      @sku_browsing_histories = SkuBrowsingHistory.limit(3).order("quantity desc").where("user_id = #{current_user.id}
+      @sku_browsing_histories = SkuBrowsingHistory.limit(3).order("updated_at desc").where("user_id = #{current_user.id}
         and sku_id in(select id from skus where status = 1 and id in(select sku_id from sku_on_shelves where status = 1))")
     end
 
