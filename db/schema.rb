3addd2840df479ecd660a600d7ac10ae1b494624
Jimmy Xu <jianmin.xu@usingnow.com>
2012-03-08 23:06:30 +0800

    refs #366
    增加注册页面的用户使用协议。需小张增加逻辑。

--- db/schema.rb
+++ db/schema.rb
@@ -946,6 +946,7 @@ ActiveRecord::Schema.define(:version => 20120308075155) do
     t.integer  "role_id"
     t.integer  "status"
     t.integer  "user_type"
+    t.string   "email",                                 :default => "", :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -958,7 +959,6 @@ ActiveRecord::Schema.define(:version => 20120308075155) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "store_id"
-    t.string   "email",                                 :default => "", :null => false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
