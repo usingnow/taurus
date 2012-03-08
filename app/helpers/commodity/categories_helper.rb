728baefbce868834433fab3e9e4711647a26764c
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-03-08 10:52:33 +0800

    fixes # 362 修复前台单品（SKU）页面，上侧导航栏，首页下一级，点击报错

--- app/helpers/commodity/categories_helper.rb
+++ app/helpers/commodity/categories_helper.rb
@@ -2,17 +2,23 @@
 module Commodity::CategoriesHelper
   def get_category_search_categories
     html = ''
-    parent = @sku_category.parent
-    if @sku_category.number.to_s.length == 4
-      html += content_tag(:h3, parent.name)
+    if @sku_category.number.to_s.length == 2
+      html += content_tag(:h3, @sku_category.name)
+      @sku_category.children.index_display.each do |child|
+        html += content_tag(:h4, (link_to child.name, commodity_category_path(child.id)), :class => "indent_20")
+        child.children.index_display.each do |grandson|
+          html += content_tag(:h4, (link_to grandson.name, commodity_category_path(grandson.id)), :class => "indent_40")
+        end
+      end
+    elsif @sku_category.number.to_s.length == 4
+      html += content_tag(:h3, @sku_category.parent.name)
       html += content_tag(:h4, (link_to @sku_category.name, commodity_category_path(@sku_category.id)), :class => "indent_20")
       @sku_category.children.index_display.each do |child|
         html += content_tag(:h4, (link_to child.name, commodity_category_path(child.id)), :class => "indent_40")
       end
     else
-      grandfather = parent.parent
-      html += content_tag(:h3, grandfather.name)
-      html += content_tag(:h4, (link_to parent.name, commodity_category_path(parent.id)), :class => "indent_20")
+      html += content_tag(:h3, @sku_category.parent.parent.name)
+      html += content_tag(:h4, (link_to @sku_category.parent.name, commodity_category_path(@sku_category.parent.id)), :class => "indent_20")
       html += content_tag(:h4, (link_to @sku_category.name, commodity_category_path(@sku_category.id)), :class => "indent_40")
     end
     html.html_safe
