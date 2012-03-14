609ca712963a0227dbaaad2b522fcab5b1d2a955
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-03-14 15:21:06 +0800

    fixes #378 前台分类搜索页面，左侧导航栏的分类显示规则需要修改

--- app/models/sku_category.rb
+++ app/models/sku_category.rb
@@ -1,6 +1,7 @@
 class SkuCategory < ActiveRecord::Base
   has_many :children, :class_name => 'SkuCategory', :foreign_key => 'parent_id'
   belongs_to :parent, :class_name => 'SkuCategory', :foreign_key => 'parent_id'
+  belongs_to :root, :class_name => 'SkuCategory', :foreign_key => 'root_id'
   scope :navigation, where(:is_show_in_navigation => true, :active => true)
   scope :top_navigation, where(:is_show_in_navigation => true, :active => true, :parent_id => nil)
 
