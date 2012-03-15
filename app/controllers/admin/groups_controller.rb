4e097e0d40dcb9073d171b03cde885a227516d6b
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-03-15 13:57:05 +0800

    fixes #381 基础管理-员工组维护，添加show功能

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -6,6 +6,10 @@ class Admin::GroupsController < ApplicationController
    @groups = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
