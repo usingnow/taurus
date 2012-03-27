a183ef74c85c1712b6e27785695a191037b60b62
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-03-27 12:25:46 +0800

    fixes #410 修改品牌信息查看版式。

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,8 +1,11 @@
+#encoding:UTF-8
 class Brand < ActiveRecord::Base
   has_many :skus
   has_attached_file :image,
                     :url  => "/paperclip_assets/brands/:id/:style_:basename.:extension",
                     :path => ":rails_root/public/paperclip_assets/brands/:id/:style_:basename.:extension"
+
+  ACTIVE = { 0 => "关闭", 1 => "激活" }
 
   validates :chinese_name, :company_name, :presence => true
 
