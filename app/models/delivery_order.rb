8a88446eb3aa9e8299622c06a48f0b2873856882
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-03-28 18:50:04 +0800

    fixes #461 调整库管系统，订单出库页面版式.

--- app/models/delivery_order.rb
+++ app/models/delivery_order.rb
@@ -11,5 +11,6 @@ class DeliveryOrder < ActiveRecord::Base
 
   validates_presence_of :store_id, :administrator_id, :name, :delivery_type, :number
   validates_presence_of :phone, :if => Proc.new { delivery_type != 1 }
-  validates_presence_of :line_type, :if => Proc.new { delivery_type == 1 }
+  validates_presence_of :line_type, :delivery_date, :if => Proc.new { delivery_type == 1 }
+  validates_presence_of :waybill_number, :if => Proc.new { line_type == 2 }
 end
