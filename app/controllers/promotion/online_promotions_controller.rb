3805fe91ae3dd3c04537b569698e0a94cb95d3a7
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-04-04 14:50:12 +0800

    fixes #482 完成促销活动记录的搜索功能

--- app/controllers/promotion/online_promotions_controller.rb
+++ app/controllers/promotion/online_promotions_controller.rb
@@ -2,7 +2,9 @@ class Promotion::OnlinePromotionsController < ApplicationController
   before_filter :authenticate_administrator!
 
   def index
-
+    @search = OnlinePromotion.search(params[:q])
+    @search.sorts = "updated_at desc"
+    @online_promotions = @search.result.paginate(:page => params[:page], :per_page => 20)
   end
 
   def search_skus
