769df8a7af05a9c71d2fe0daf6ebf99c8fd98967
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-04-19 15:45:29 +0800

    fixes #537 修改订单打印为所有站别都可配置权限。

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,15 +1,18 @@
 class Procedure < ActiveRecord::Base
   has_many :station_procedureships
-  has_many :stations, :through => :station_procedureships
+
   scope :activated, where(:active => true)
 
   def self.user_procedures(user_id)
     where("id in(select procedure_id from procedure_roleships where role_id in(select role_id from users where id = #{user_id}))")
   end
 
-  def normal_stations
-    stations.normal_stations.group(:id)
+  def stations
+    Station.find_by_sql("SELECT `stations`.* FROM `stations` INNER JOIN `station_procedureships` ON
+                         `stations`.`id` = `station_procedureships`.`next_station_id` WHERE
+                         `station_procedureships`.`procedure_id` = #{id} AND (station_type in(1,2,4)) GROUP BY id")
   end
+
 
   validates :name, :display_name, :presence => true
   validates_numericality_of :sequence, :only_integer => true, :message => :not_an_integer
