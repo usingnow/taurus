d9136fc250e8054f35920b5f79960a704a6e3cd9
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-06-25 18:54:13 +0800

    fixes #784 组合商品管理，填写商品分类时，提示文字修改

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -15,6 +15,7 @@ Spork.prefork do
 	require "ffaker"
 	require "shoulda-matchers"
   require 'rspec/autorun'
+  require 'factory_girl'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
