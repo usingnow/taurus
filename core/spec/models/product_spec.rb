c8e215f1526fc42184b60c17f2bedf6c09ed672a
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-06-25 17:34:32 +0800

    fixes #787 销售商品列表页面增加显示销售状态与是否上架

--- core/spec/models/product_spec.rb
+++ core/spec/models/product_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe Taurus::Product do
+  before(:each) do
+    @product = Taurus::Product.new
+    @product.number = 'ebook_001'
+		@product.name = 'test_ebook'
+		@product.product_category_name = "test_category"
+		@product.product_category_id = '24'
+		@product.market_price = '32.5'
+		@product.price_after_tax = '16.48'
+		@product.weight = '0.0'
+		@product.sales_status = 1
+		@product.if_shown_on_web = 1
+		@product.product_type = 0
 
+		@product.save
+  end
+  
   context "validation" do
 
     it { should validate_presence_of(:number) }
@@ -11,20 +26,7 @@ describe Taurus::Product do
     it { should validate_presence_of(:weight) }
     it { should validate_presence_of(:price_after_tax) }
     
-    it { 
-      Taurus::Product.create!(:number => 'ebook_001',
-														  :name => 'test_ebook',
-														  :product_category_name => "test_category",
-														  :product_category_id => '24',
-														  :market_price => '32.5',
-														  :price_after_tax => '16.48',
-														  :weight => '0.0',
-														  :sales_status => 1,
-														  :if_shown_on_web => 1,
-														  :product_type => 0)
-
-      should validate_uniqueness_of(:number) 
-    }
+    it { should validate_uniqueness_of(:number) }
 
   end
 end
