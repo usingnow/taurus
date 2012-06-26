4e535281bd2428334c20977e3be50ddf86fef2c1
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-06-26 15:14:17 +0800

    fixes #751 增加商品管理页面，查看全部销售单品与全部组合商品按钮。

--- core/lib/taurus/core/testing_support/common_rake.rb
+++ core/lib/taurus/core/testing_support/common_rake.rb
@@ -6,7 +6,7 @@ desc "Generates a dummy app for testing"
 namespace :common do
   task :test_app do
     require "#{ENV['LIB_NAME']}"
-
+    
     Taurus::DummyGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--database=#{ENV['DB_NAME']}", "--quiet"]
     Taurus::InstallGenerator.start ["--lib_name=#{ENV['LIB_NAME']}", "--auto-accept", "--migrate=false", "--seed=false", "--sample=false", "--quiet"]
 
@@ -20,5 +20,6 @@ namespace :common do
     end
 
     system(cmd)
+    puts "abc"
   end
 end
