a391bb138815ec6ad6a6aa23551455745561b787
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-06-26 13:38:02 +0800

    fixes #722 修改收货人信息验证，固定电话与移动电话二选一。

--- core/app/models/taurus/order_delivery.rb
+++ core/app/models/taurus/order_delivery.rb
@@ -4,5 +4,11 @@ module Taurus
   	belongs_to :district
 
   	validates_presence_of :district_id, :attention, :address
+  	validate :phone_or_mobile
+
+  	protected
+  	def phone_or_mobile
+      errors.add(:telephone, :phone_or_mobile) if telephone.blank? && mobile.blank?
+  	end
   end
 end
