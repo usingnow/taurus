e0cad2ceee7c55b1cd3f73a8f96272970f229a61
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-06-27 11:26:08 +0800

    fixes #793 订单模块增加出库操作按钮

--- core/app/helpers/taurus/orders_helper.rb
+++ core/app/helpers/taurus/orders_helper.rb
@@ -9,14 +9,19 @@ module Taurus
       Order::STATE[state]
 		end
 
-		def order_operations(order)
+		def order_operations(order, html_class = "")
 			buttons = ''
       order.state_events.each do |event|
       	if event != :online_payment && event != :sign && event != :product_delivery
 	        buttons << (
 	        	link_to format_order_enent(event), eval("#{event.to_s}_admin_order_path(order)"), 
-	        	        :class => "btn btn-primary trk-btn-short"
+	        	        :class => "btn btn-primary " + html_class
 	      	)
+	      elsif event == :product_delivery	
+	      	buttons << (
+		      	link_to format_order_enent(event), new_admin_order_delivery_record_path(:order_id => order), 
+		        	      :class => "btn btn-primary " + html_class, :target => "_blank"
+		      )  	      
       	end
       end
       buttons.html_safe
