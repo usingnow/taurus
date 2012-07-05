be1973a7e06fe0d394ce5719c90e970419f38807
Zhang Xiangyu <xiangyu.zhang@usingnow.com>
2012-07-05 16:20:20 +0800

    mobile test

--- auth/app/controllers/taurus/admin/admin_sessions_controller.rb
+++ auth/app/controllers/taurus/admin/admin_sessions_controller.rb
@@ -3,6 +3,22 @@ module Taurus
     class AdminSessionsController < Devise::SessionsController
       layout '/taurus/layouts/devise'
 
+      def create
+        respond_to do |format|
+          format.json do
+            resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+            sign_in(resource_name, resource)
+            render :json => {:success => true, :content => "abc"}
+          end
+
+          format.html { super }
+        end
+      end
+
+      def failure
+        render :json => {:success => false, :errors => ["Login failed."]}
+      end
+
       protected
       def after_sign_in_path_for(resource_or_scope)
         "/admin"
